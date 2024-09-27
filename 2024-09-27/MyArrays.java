802dfb81fbb3c0397d9c2f4680fd239d15372dc0
Andy Li <ali60@stuy.edu>
2024-09-27 10:55:40 -0400

    Added the structure of the returnCopy function test

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,1 +1,4 @@
-
+public static void main(String[] args){
+    int[] toTest;
+    System.out.println("Expected true and function returned " + (toTest != returnCopy(toTest) && arrayToString(toTest).equals(arrayToString(returnCopy(toTest)))));
+}
