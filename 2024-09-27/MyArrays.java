1d2de6ebad910789da8c54208177860d1e705c2b
Andy Li <ali60@stuy.edu>
2024-09-27 10:58:44 -0400

    Added the arrayToString method

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -2,3 +2,15 @@ public static void main(String[] args){
     int[] toTest;
     System.out.println("Expected true and function returned " + (toTest != returnCopy(toTest) && arrayToString(toTest).equals(arrayToString(returnCopy(toTest)))));
 }
+
+public static String arrayToString(int[] nums){
+    String result = "[";
+    for (int count = 0; count < nums.length; count++){
+        result += nums[count];    
+        if (count < nums.length - 1){
+            result += ", "        
+        }
+    }
+    result += "]";
+    return result;
+}
