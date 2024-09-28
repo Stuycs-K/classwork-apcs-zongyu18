8634cf4b0a96e94f70db19fc878f3a75c5c3f228
Andy Li <ali60@stuy.edu>
2024-09-27 22:12:31 -0400

    added 6 test cases for the returnCopy function

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,6 +1,17 @@
 public static void main(String[] args){
     int[] toTest;
+	toTest = [];
     System.out.println("Expected true and function returned " + (toTest != returnCopy(toTest) && arrayToString(toTest).equals(arrayToString(returnCopy(toTest)))));
+	toTest = ["Hello"];
+	System.out.println("Expected true and function returned " + (toTest != returnCopy(toTest) && arrayToString(toTest).equals(arrayToString(returnCopy(toTest)))));
+	toTest = ["Hello", "World"];
+	System.out.println("Expected true and function returned " + (toTest != returnCopy(toTest) && arrayToString(toTest).equals(arrayToString(returnCopy(toTest)))));
+	toTest = ["1", "2", "3"];
+	System.out.println("Expected true and function returned " + (toTest != returnCopy(toTest) && arrayToString(toTest).equals(arrayToString(returnCopy(toTest)))));
+	toTest = ["1", "Hello", "World"];
+	System.out.println("Expected true and function returned " + (toTest != returnCopy(toTest) && arrayToString(toTest).equals(arrayToString(returnCopy(toTest)))));
+	toTest = ["\n", "\\"];
+	System.out.println("Expected true and function returned " + (toTest != returnCopy(toTest) && arrayToString(toTest).equals(arrayToString(returnCopy(toTest)))));
 }
 
 public static String arrayToString(int[] nums){
