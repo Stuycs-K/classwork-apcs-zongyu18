2f726f41724f360ab4b7299c6fd1628d2e708bd0
Andy Li <ali60@stuy.edu>
2024-09-28 13:55:44 -0400

    added 8 test cases for the concatArray function

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -2,21 +2,40 @@ public static void main(String[] args){
     int[] testReturnCopy;
 	int[] testConcatArrayA;
 	int[] testConcatArrayB;
-	testReturnCopy = {};
-    System.out.println("Expected arrays matching but not the same true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
+	testReturnCopy = new int[]{};
+    System.out.println("Expected arrays matching but not the same to return true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
 	testReturnCopy = new int[0];
-    System.out.println("Expected arrays matching but not the same true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
+    System.out.println("Expected arrays matching but not the same to return true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
 	testReturnCopy = new int[3];
-    System.out.println("Expected arrays matching but not the same true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
-	testReturnCopy = {1};
-	System.out.println("Expected arrays matching but not the same true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
-	testReturnCopy = {0, 1};
-	System.out.println("Expected arrays matching but not the same true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
-	testReturnCopy = {1, 0};
-	System.out.println("Expected arrays matching but not the same true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
-	testReturnCopy = {1, 2, 3};
-	System.out.println("Expected arrays matching but not the same true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
-	testConcatArrayA = 
+    System.out.println("Expected arrays matching but not the same to return true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
+	testReturnCopy = new int[]{1};
+	System.out.println("Expected arrays matching but not the same to return true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
+	testReturnCopy = new int[]{0, 1};
+	System.out.println("Expected arrays matching but not the same to return true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
+	testReturnCopy = new int[]{1, 0};
+	System.out.println("Expected arrays matching but not the same to return true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
+	testReturnCopy = new int[]{1, 2, 3};
+	System.out.println("Expected arrays matching but not the same to return true and function returned " + (testReturnCopy != returnCopy(testReturnCopy) && arrayToString(testReturnCopy).equals(arrayToString(returnCopy(testReturnCopy)))));
+	testConcatArrayA = new int[0];
+	testConcatArrayB = new int[]{};
+	System.out.println("Expected concatened array to be [] and function returned " + concatArray(testConcatArrayA, testConcatArrayB) + "; expected equals returned is " + arrayToString(concatArray(testConcatArrayA, testConcatArrayB)).equals("[]"));
+	testConcatArrayB = new int[]{1};
+	System.out.println("Expected concatened array to be [1] and function returned " + concatArray(testConcatArrayA, testConcatArrayB) + "; expected equals returned is " + arrayToString(concatArray(testConcatArrayA, testConcatArrayB)).equals("[1]"));
+	testConcatArrayA = new int[]{1};
+	System.out.println("Expected concatened array to be [1, 1] and function returned " + concatArray(testConcatArrayA, testConcatArrayB) + "; expected equals returned is " + arrayToString(concatArray(testConcatArrayA, testConcatArrayB)).equals("[1, 1]"));
+	testConcatArrayA = new int[]{123, 456, 7890};
+	testConcatArrayB = new int[]{};
+	System.out.println("Expected concatened array to be [123, 456, 7890] and function returned " + concatArray(testConcatArrayA, testConcatArrayB) + "; expected equals returned is " + arrayToString(concatArray(testConcatArrayA, testConcatArrayB)).equals("[123, 456, 7890]"));
+	testConcatArrayB = new int[0];
+	System.out.println("Expected concatened array to be [123, 456, 7890] and function returned " + concatArray(testConcatArrayA, testConcatArrayB) + "; expected equals returned is " + arrayToString(concatArray(testConcatArrayA, testConcatArrayB)).equals("[123, 456, 7890]"));
+	testConcatArrayB = new int[]{2, 3};
+	System.out.println("Expected concatened array to be [1, 2, 3] and function returned " + concatArray(testConcatArrayA, testConcatArrayB) + "; expected equals returned is " + arrayToString(concatArray(testConcatArrayA, testConcatArrayB)).equals("[1, 2, 3]"));
+	testConcatArrayA = new int[]{2, 18};
+	testConcatArrayB = new int[]{2008};
+	System.out.println("Expected concatened array to be [2, 18, 2008] and function returned " + concatArray(testConcatArrayA, testConcatArrayB) + "; expected equals returned is " + arrayToString(concatArray(testConcatArrayA, testConcatArrayB)).equals("[2, 18, 2008]"));
+	testConcatArrayA = new int[]{2008};
+	testConcatArrayB = new int[]{2, 18};
+	System.out.println("Expected concatened array to be [2008, 2, 18] and function returned " + concatArray(testConcatArrayA, testConcatArrayB) + "; expected equals returned is " + arrayToString(concatArray(testConcatArrayA, testConcatArrayB)).equals("[2008, 2, 18]"));
 }
 
 public static String[] arrayToString(int[] nums){
