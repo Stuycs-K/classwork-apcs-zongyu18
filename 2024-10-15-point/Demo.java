d5778118d6d1ef8663b7bff8b9bccfd0047b44f9
Andy Li <ali60@stuy.edu>
2024-10-15 10:58:39 -0400

    fixed the distance method

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -5,7 +5,7 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return (Math.pow(Math.pow(a, 2) + Math.pow(b, 2)), 0.5);
+    return Math.pow((Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2)), 0.5);
   }
 
   public static void main(String[]args){
