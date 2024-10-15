9ca42ff2435b29e32127a6e63975ea950573e302
Andy Li <ali60@stuy.edu>
2024-10-15 10:51:37 -0400

    coded the toString method and the Point constructor, and tested them

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -1,1 +1,24 @@
+public class Demo{
 
+  /*
+  Write this method third. Test it then move on
+  to the others.
+  */
+  public static double distance(Point a, Point b){
+    return 0.0;
+  }
+
+  public static void main(String[]args){
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(-1,-1);
+    Point p3 = new Point(3,4);
+    System.out.println( p3);
+    Point p4 = new Point(p3);
+    System.out.println(p4.toString());
+    System.out.println( distance(p1,p2));
+    System.out.println( Point.distance(p1,p2));
+    System.out.println( p1.distanceTo(p2));
+
+
+  }
+}
--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -1,1 +1,42 @@
+public class Point{
+  private double x,y;
 
+  public String toString(){
+    return "(" + x + ", " + y + ")";
+  }
+
+  //Initialize this Point to have the same values as the other Point.
+  public Point(Point other){
+    x = other.x;
+    y = other.y;
+  }
+
+  //Initialize this Point to have the provided values
+  public Point(double x, double y){
+    this.x=x;
+    this.y=y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+
+  /*
+  Write this method FOURTH, go to the main and do that one first.
+  */
+  public double distanceTo(Point other){
+    return 0.0;
+  }
+
+  /*
+  Write this method last. Re-use (NOT COPY/PASTE) prior work and
+  do not write redundant code.
+  */
+  public static double distance(Point a, Point b){
+    return 0.0;
+  }
+
+
+}
