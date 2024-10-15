8683455583f28dc9789a83359024f87211004efe
Andy Li <ali60@stuy.edu>
2024-10-15 10:53:21 -0400

    coded the distance method

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -5,7 +5,7 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return 0.0;
+    return (Math.pow(Math.pow(a, 2) + Math.pow(b, 2)), 0.5);
   }
 
   public static void main(String[]args){
