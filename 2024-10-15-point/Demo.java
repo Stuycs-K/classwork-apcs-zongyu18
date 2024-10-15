1d315fdbb338256db78aecdfcb442e055e6fc60f
Andy Li <ali60@stuy.edu>
2024-10-15 11:00:39 -0400

    added one more test case for the distance method using (3,4) and the origin

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -14,9 +14,11 @@ public class Demo{
     Point p3 = new Point(3,4);
     System.out.println( p3);
     Point p4 = new Point(p3);
+    Point origin = new Point(0, 0);
     System.out.println(p4.toString());
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
+    System.out.println(distance(p3, origin));
     System.out.println( p1.distanceTo(p2));
 
 
