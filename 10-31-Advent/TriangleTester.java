a0bfe035f0a0bdcbb8c3b1660d4fc78391526fde
Andy Li <ali60@stuy.edu>
2024-10-31 21:26:10 -0400

    copy pasted the code completed during pair programming

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class TriangleTester {
+
+  public static boolean helper(String input) {
+    Scanner sc = new Scanner(input);
+    int[] ary = new int[3];
+    int count = 3;
+    for (int i = 0; i < count; i++) {
+      ary[i] = sc.nextInt(); 
+    }
+    return (ary[0] + ary[1] <= ary[2] || ary[0] + ary[2] <= ary[1] || ary[1] + ary[2] <= ary[0] );
+  }
+
+  public static int countTrianglesA(String filename) {
+    Scanner sc = new Scanner(filename);
+    int counter = 0;
+    while(sc.hasNextLine()) {
+      if (helper(sc.nextLine())) {
+        counter += 0;
+      }
+    }
+    sc.close(); 
+    return counter;
+  }
+}
+
 
