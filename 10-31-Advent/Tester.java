38fc95f9c3e1b604e2d9776eea46cf9e8fe1e73a
Andy Li <ali60@stuy.edu>
2024-11-01 23:18:02 -0400

    successfully coded the countTrianglesA method and tested it with the initial 5 test cases

--- 10-31-Advent/Tester.java
+++ 10-31-Advent/Tester.java
@@ -1,9 +1,12 @@
-import java.io.*;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Tester{
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		//2 Opening a file requires a try/catch
+
 		// Helper Tests
 		int counter = 0;
 		if (!TriangleTester.helper(5, 10, 25)){
@@ -22,7 +25,7 @@ public class Tester{
 			counter++;
 		}
 		System.out.println(counter + " helper tests passed out of 5 total");
-		
+		System.out.println(TriangleTester.countTrianglesA("input.txt"));
 		
 	}
 }
--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,4 +1,4 @@
-import java.io.*;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -8,18 +8,34 @@ public class TriangleTester {
   public static boolean helper(int lengthA, int lengthB, int lengthC){
 	return !(lengthA + lengthB <= lengthC || lengthB + lengthC <= lengthA || lengthA + lengthC <= lengthB);
   }
-/*
-  public static int countTrianglesA(String filename){
-    Scanner sc = new Scanner(filename);
-    int counter = 0;
-    while(sc.hasNextLine()) {
-      if (helper(sc.nextLine())) {
-        counter += 0;
-      }
+
+  public static int countTrianglesA(String filename) throws FileNotFoundException
+  {
+	int counter = 0;
+    //2 Opening a file requires a try/catch
+    try {
+      File file = new File("inputTri.txt");//1
+      Scanner input = new Scanner(file);
+      //CODE THAT SCANS THE FILE.
+      input.close();//releases the file from your program
+
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
     }
-    sc.close(); 
+	File file = new File("inputTri.txt");//1
+    Scanner input = new Scanner(file);
+	String lengthSet;
+	String[] lengthArray = new String[3];
+	while (input.hasNextLine()){
+		lengthSet = input.nextLine();
+		lengthArray = lengthSet.split(" ");
+		if (helper(Integer.parseInt(lengthArray[0]), Integer.parseInt(lengthArray[1]), Integer.parseInt(lengthArray[2]))){
+			counter++;
+		} 
+	}
     return counter;
-  }*/
+  }
 }
 
 
