671392e81286649f7e552ffaa6ca5160a15cd4e6
Andy Li <ali60@stuy.edu>
2024-11-07 11:01:04 -0500

    finished the rest of the steps (calling the three new speaks)

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,6 +1,6 @@
 // speak() is overridden in the child class but not the parent, same with getName()
 //Therefore, the String "The Mighty"  is added before the original getName() method
-
+// We see this in speak() as well where the extra two lines of the Bird class are now printed
 // The first two constructors work because they are creating an object of the same type
 //The third one does not work because an Animal is not necessarily a Bird (Animal does not have all the fields Bird does)
 //The last one does work because a Bird is an animal (i.e. Animal has Bird)
@@ -16,5 +16,8 @@ public class Driver{
     Bird b1 = new Bird("bark", 56, "Snoopy", 63, "white");
     //Bird b2 = new Animal("moo", 14, "Cow");
     Animal a2 = new Bird("chirpchirp", 2, "Toucannon", 43, "black");
+    a1.speak();
+    b1.speak();
+    a2.speak();
   }
 }
