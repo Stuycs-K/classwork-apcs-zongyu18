7c8f0e75cbf40724f7b9c9f934b78ffd08d4569c
Andy Li <ali60@stuy.edu>
2024-11-18 10:57:05 -0500

    finished the code for Day 1

--- Advent.java
+++ Advent.java
@@ -12,13 +12,33 @@ public class Advent{
       File file = new File("input.txt");
       Scanner input = new Scanner(file);
       while (input.hasNext()){
-        if (input.next().substring(0, 1).equals("L")){
+        direction += 360;
+        String value = input.next();
+        if (value.substring(0, 1).equals("L")){
           direction -= 90;
         }
         else{
           direction += 90;
         }
+        direction %= 360;
+        if (direction == 0){
+          vertical += value.substring(1, value.length() - 1);
+        }
+        else if (direction == 90){
+          horizontal += value.substring(1, value.length() - 1);
+        }
+        else if (direction == 180){
+          vertical -= value.substring(1, value.length() - 1);
+        }
+        else{
+          horizontal -= value.substring(1, value.length() - 1);
+        }
       }
+      return horizontal + vertical;
+    } catch (FileNotFoundException e) {
+      //File not found what should you do?
+      System.out.println("File not found");
+	     return 0;
     }
   }
 }
