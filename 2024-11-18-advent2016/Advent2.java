6cc710b6675e35618c48b85aa754301b4008abdd
Andy Li <ali60@stuy.edu>
2024-11-19 19:31:49 -0500

    successfully adapted Day Six Part A's code for Part B

--- 2024-11-18-advent2016/Advent2.java
+++ 2024-11-18-advent2016/Advent2.java
@@ -45,8 +45,51 @@ public static String daySix(String fileName){
      return "fail";
   }
 }
+
+public static String daySixHelperB(String fileName, int index){
+  try{
+    int toReturnHelp = 10000;
+	int tracker = 0;
+    File file = new File("input.txt");
+    Scanner input = new Scanner(file);
+    int[] alphabet = new int[26];
+    while (input.hasNextLine()){
+      String line = input.nextLine();
+      alphabet[line.charAt(index) - 97] += 1;
+    }
+    for (int count = 0; count < 26; count++){
+      if (alphabet[count] < toReturnHelp && alphabet[count] != 0){
+        toReturnHelp = alphabet[count];
+		tracker = count;
+      }
+    }
+    return "" + (char)(tracker+97);
+  }
+  catch (FileNotFoundException e) {
+    //File not found what should you do?
+    System.out.println("File not found");
+     return "fail";
+  }
+}
+
+public static String daySixB(String fileName){
+  try{
+    File file = new File("input.txt");
+	String toReturn = "";
+    Scanner input = new Scanner(file);
+    for (int count = 0; count < input.nextLine().length(); count++){
+		toReturn += daySixHelperB(fileName, count);
+	}
+	return toReturn;
+  }
+  catch (FileNotFoundException e) {
+    //File not found what should you do?
+    System.out.println("File not found");
+     return "fail";
+  }
+}
 	
   public static void main(String[] args){
-    System.out.println(daySix("input.txt"));
+    System.out.println(daySixB("input.txt"));
   }
 }
