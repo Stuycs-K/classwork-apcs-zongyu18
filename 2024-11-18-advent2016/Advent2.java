3cde40ff61ecc60c27054d92bb7a0500225447bb
Andy Li <ali60@stuy.edu>
2024-11-19 19:24:41 -0500

    fixed some small errors in helper method, coded main method, and imported scanner class

--- 2024-11-18-advent2016/Advent2.java
+++ 2024-11-18-advent2016/Advent2.java
@@ -1,20 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Advent2{
   public static String daySixHelper(String fileName, int index){
   try{
     int toReturnHelp = 0;
+	int tracker = 0;
     File file = new File("input.txt");
     Scanner input = new Scanner(file);
-    int[] alphabet = new int[](26);
+    int[] alphabet = new int[26];
     while (input.hasNextLine()){
       String line = input.nextLine();
-      alphabet[line.charAt(index) - 97)] += 1;
+      alphabet[line.charAt(index) - 97] += 1;
     }
     for (int count = 0; count < 26; count++){
       if (alphabet[count] > toReturnHelp){
         toReturnHelp = alphabet[count];
+		tracker = count;
       }
     }
-    return "" + (char)toReturnHelp;
+    return "" + (char)(tracker+97);
   }
   catch (FileNotFoundException e) {
     //File not found what should you do?
@@ -22,7 +28,25 @@ public class Advent2{
      return "fail";
   }
 }
+
+public static String daySix(String fileName){
+  try{
+    File file = new File("input.txt");
+	String toReturn = "";
+    Scanner input = new Scanner(file);
+    for (int count = 0; count < input.nextLine().length(); count++){
+		toReturn += daySixHelper(fileName, count);
+	}
+	return toReturn;
+  }
+  catch (FileNotFoundException e) {
+    //File not found what should you do?
+    System.out.println("File not found");
+     return "fail";
+  }
+}
+	
   public static void main(String[] args){
-    System.out.println(dayFour("input.txt"));
+    System.out.println(daySix("input.txt"));
   }
 }
