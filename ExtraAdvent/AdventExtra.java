e2a28c368ffb4510b3e9ee5de329d60b7ed27b93
Andy Li <ali60@stuy.edu>
2024-11-20 20:48:40 -0500

    finished the initial code for Day 7 part 2

--- ExtraAdvent/AdventExtra.java
+++ ExtraAdvent/AdventExtra.java
@@ -64,8 +64,70 @@ public class AdventExtra{
   }
   }
   
+  public static boolean daySevenHelperB(String toRead){
+	  boolean truthValue = false;
+	  String checker = "";
+	  String brackets = "";
+	  String outsideBrackets = "";
+	  for (int count = 0; count < toRead.length(); count++){
+		  if (truthValue){
+			  if (toRead.substring(count, count + 1).equals("]")){
+				  for (int innerCount = 0; innerCount <= checker.length() - 3; innerCount++){
+					  if (checker.substring(innerCount, innerCount + 3).equals(reverse(checker.substring(innerCount, innerCount+3))) 
+			&& !checker.substring(innerCount, innerCount+1).equals(checker.substring(innerCount+1,innerCount+2))){
+						  brackets += checker.substring(innerCount, innerCount+3) + " ";
+					  }
+				  }
+				  checker = "";
+				  truthValue = false;
+			  }
+			  else{
+				  checker += toRead.substring(count, count + 1);
+			  }
+		  }
+		  else{
+			  if (toRead.substring(count, count + 1).equals("[")){
+				  truthValue = true;
+			  }
+		  }
+	  }
+	  truthValue = false;
+	for (int count = 0; count < toRead.length() - 3; count++){
+	
+		if (truthValue && toRead.substring(count, count + 3).equals(reverse(toRead.substring(count, count+3))) 
+			&& !toRead.substring(count, count+1).equals(toRead.substring(count+1,count+2))){
+			outsideBrackets += toRead.charAt(count + 1) + toRead.charAt(count) + toRead.charAt(count+1) + " ";
+		}
+		
+	}
+     for (int count = 0; count < outsideBrackets.length(); count += 4){
+		 if (brackets.contains(outsideBrackets.substring(count, count +4))){
+			 return true;
+		 }
+	 }
+	 return false;
+    
+  }
+  public static int daySevenB(String filename){
+	  try{
+		  int toReturn = 0;
+		  File file = new File("input.txt");
+		  Scanner input = new Scanner(file);
+		  while (input.hasNextLine()){
+			  if (daySevenHelperB(input.nextLine())){
+				  toReturn++;
+			  }
+		  }
+		  return toReturn;
+	  }catch (FileNotFoundException e) {
+    //File not found what should you do?
+    System.out.println("File not found");
+     return 10;
+  }
+  }
+  
   public static void main(String[] args){
-	  System.out.println(daySeven("input.txt"));
+	  System.out.println(daySevenB("input.txt"));
   }
 }
   
