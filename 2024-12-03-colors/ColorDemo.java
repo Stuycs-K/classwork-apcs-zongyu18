27ca872d9051b5b73b5ced1c8bf2618130642313
Andy Li <ali60@stuy.edu>
2024-12-03 13:22:13 -0500

    coded a fun little number guessing game implementing 3 different text colors based on whether the answer is right/wrong and if the user got lucky first try (1% chance!!)

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ColorDemo{
   public static void main(String args[]){
     System.out.print("\u001b[31m");
@@ -19,6 +21,48 @@ public class ColorDemo{
     System.out.println("\u001b[0m");
     System.out.print("\u001b[4;1;40;37m");
     System.out.println("This is white, bolded, and underlined text on a black background");
-    System.out.print("\u001b[0m");
+    System.out.println("\u001b[0m");
+    System.out.print("\u001b[1;37;4m");
+    System.out.print("LET'S PLAY A GAME");
+    System.out.println("\u001b[0m");
+    int answer = (int) (Math.random() * 101);
+    int counter = 0;
+    boolean incorrect = true;
+    System.out.println(answer);
+    while (counter < 6 && incorrect){
+      counter++;
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter a number from 0 to 100! ");
+      String guess = input.nextLine();
+      int guessInt = Integer.parseInt(guess);
+      if (guessInt == answer){
+        if (counter == 1){
+          System.out.println("\u001b[93m");
+          System.out.println("LUCKY WINNER! You got the answer in " + counter + " try!");
+          System.out.println("\u001b[0m");
+        }
+        else{
+          System.out.println("\u001b[92m");
+          System.out.println("CORRECT! You got the answer in " + counter + " tries!");
+          System.out.println("\u001b[0m");
+        }
+        incorrect = false;
+      }
+      else if (guessInt < answer){
+        System.out.println("\u001b[31m");
+        System.out.println("WRONG! Your answer is too low :(");
+        System.out.println("\u001b[0m");
+      }
+      else{
+        System.out.println("\u001b[31m");
+        System.out.println("WRONG! Your answer is too high :(");
+        System.out.println("\u001b[0m");
+      }
+    }
+    if (incorrect){
+      System.out.println("\u001b[31m");
+      System.out.println("FAIL! Your six tries are over! The correct answer was " + answer +".");
+      System.out.println("\u001b[0m");
+    }
   }
 }
