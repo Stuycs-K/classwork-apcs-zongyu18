8a149acbfc4afe2abfb29e70c6f358dbc8ed5fa4
Andy Li <ali60@stuy.edu>
2024-12-03 11:16:49 -0500

    added more text colors, background colors, underlined/italicized text, and a reset line

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -6,5 +6,15 @@ public class ColorDemo{
     System.out.println("This color is orange");
     System.out.print("\u001b[93m");
     System.out.println("This color is bright yellow");
+    System.out.print("\u001b[30m");
+    System.out.print("\u001b[44m");
+    System.out.println("This background color is blue");
+    System.out.print("\u001b[45m");
+    System.out.println("This background color is magenta");
+    System.out.print("\u001b[106m");
+    System.out.println("This background color is bright cyan");
+    System.out.print("\u001b[4; 1; 100; 37]");
+    System.out.println("This is white, bolded, and underlined text on a black background");
+    System.out.print("u001b[0m");
   }
 }
