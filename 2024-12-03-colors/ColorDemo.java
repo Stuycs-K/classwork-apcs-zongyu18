acc6993da3e7c799c29dfc52319ad4e097b99eda
Andy Li <ali60@stuy.edu>
2024-12-03 13:00:49 -0500

    fixed some minor issues with the previous code

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -3,18 +3,22 @@ public class ColorDemo{
     System.out.print("\u001b[31m");
     System.out.println("This color is red");
     System.out.print("\u001b[32m");
-    System.out.println("This color is orange");
+    System.out.println("This color is green");
     System.out.print("\u001b[93m");
     System.out.println("This color is bright yellow");
     System.out.print("\u001b[30m");
     System.out.print("\u001b[44m");
-    System.out.println("This background color is blue");
+    System.out.print("This background color is blue");
+    System.out.println("\u001b[0m");
     System.out.print("\u001b[45m");
-    System.out.println("This background color is magenta");
+    System.out.print("This background color is magenta");
+    System.out.println("\u001b[0m");
     System.out.print("\u001b[106m");
-    System.out.println("This background color is bright cyan");
-    System.out.print("\u001b[4; 1; 100; 37]");
+    System.out.print("\u001b[30m");
+    System.out.print("This background color is bright cyan");
+    System.out.println("\u001b[0m");
+    System.out.print("\u001b[4;1;40;37m");
     System.out.println("This is white, bolded, and underlined text on a black background");
-    System.out.print("u001b[0m");
+    System.out.print("\u001b[0m");
   }
 }
