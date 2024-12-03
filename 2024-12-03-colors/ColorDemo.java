bc082e3fe141d67cca97ca55f9c6dc0788537369
Andy Li <ali60@stuy.edu>
2024-12-03 11:11:34 -0500

    added a few lines with different colors

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,10 @@
+public class ColorDemo{
+  public static void main(String args[]){
+    System.out.print("\u001b[31m");
+    System.out.println("This color is red");
+    System.out.print("\u001b[32m");
+    System.out.println("This color is orange");
+    System.out.print("\u001b[93m");
+    System.out.println("This color is bright yellow");
+  }
+}
