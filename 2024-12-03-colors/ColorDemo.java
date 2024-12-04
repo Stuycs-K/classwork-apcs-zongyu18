1caa55d46b3e41b0d475c03437822a383172bce6
Andy Li <ali60@stuy.edu>
2024-12-04 00:30:54 -0500

    added a red blue gradient and tweaked the random number guesser code

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -22,14 +22,27 @@ public class ColorDemo{
     System.out.print("\u001b[4;1;40;37m");
     System.out.println("This is white, bolded, and underlined text on a black background");
     System.out.println("\u001b[0m");
-    System.out.print("\u001b[1;37;4m");
-    System.out.print("LET'S PLAY A GAME");
     System.out.println("\u001b[0m");
+    Scanner inputB = new Scanner(System.in);
+    System.out.println("Enter a number! ");
+    String number = inputB.nextLine();
+    int numberParsed = Integer.parseInt(number) % 20 + 12;
+    for(int red = 0; red < 256; red+=numberParsed){
+      for(int blue = 0; blue <= 256; blue+=numberParsed){
+          System.out.print("\u001b[38;2;"+red+";"+0+";"+blue+";7m.");
+
+  }
+  System.out.println("\u001b[0m");
+
+}
+System.out.print("\u001b[1;37;4m");
+System.out.print("LET'S PLAY A GAME");
+System.out.println("\u001b[0m");
+
     int answer = (int) (Math.random() * 101);
     int counter = 0;
     boolean incorrect = true;
-    System.out.println(answer);
-    while (counter < 6 && incorrect){
+    while (counter < 7 && incorrect){
       counter++;
       Scanner input = new Scanner(System.in);
       System.out.println("Enter a number from 0 to 100! ");
@@ -61,7 +74,7 @@ public class ColorDemo{
     }
     if (incorrect){
       System.out.println("\u001b[31m");
-      System.out.println("FAIL! Your six tries are over! The correct answer was " + answer +".");
+      System.out.println("FAIL! Your seven tries are used up! The correct answer was " + answer +".");
       System.out.println("\u001b[0m");
     }
   }
