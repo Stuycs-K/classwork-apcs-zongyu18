f9fb85fa3f9fa91458245a5d11f12b37f5321a8c
Andy Li <ali60@stuy.edu>
2024-12-04 23:39:10 -0500

    coded the random array and printing code

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -22,8 +22,33 @@ public class Driver extends Text{
     }
   }
 
+  public static void partSixToEight(){
+    int[] arr = new int[3];
+    for (int count = 0; count < 3; count++){
+      int input = (int) (Math.random() * 100);
+      arr[count] = input;
+    }
+    int increment = 0;
+    for (int secondCount = 0; secondCount < 3; secondCount++){
+      increment += 20;
+      go(2, increment);
+      if (arr[secondCount] < 25){
+        color(RED, background(BLACK), BRIGHT);
+      }
+      else if (arr[secondCount] > 75){
+        color(GREEN, background(BLACK), BRIGHT);
+      }
+      else{
+        color(WHITE, background(BLACK));
+      }
+      System.out.print(arr[secondCount]);
+      System.out.print(RESET);
+    }
+  }
+
   public static void main(String[] args){
     System.out.println(CLEAR_SCREEN);
     partFive();
-    }
+    partSixToEight();
   }
+}
