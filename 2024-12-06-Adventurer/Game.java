aacf2e3fcd8c5569cd1a063b0bfb1eb022bcae37
Andy Li <ali60@stuy.edu>
2024-12-09 10:56:57 -0500

    condensed the loop into a helper so I could recursive call it until the user enters a valid input

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,4 +1,24 @@
 public class Game{
+  public static void helper(){
+    System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+    response = userInput.nextLine();
+    response.toLowerCase();
+    if (response.equals("a") || response.equals("attack")){
+      player.attack(enemy);
+    }
+    else if (response.equals("sp") || response.equals("special")){
+      player.specialAttack(enemy);
+    }
+    else if (response.equals("su") || response.equals("support")){
+      player.support()
+    }
+    else if (response.equals("quit")){
+      return;
+    }
+    else{
+      helper();
+    }
+  }
   public static void main(String[] args){
     Scanner userInput = new Scanner(System.in);
     System.out.println("Enter player name below");
@@ -9,16 +29,9 @@ public class Game{
       System.out.println(player.getName() + " / " + player.getHP() + " / " + player.getSpecial() + " " + player.getSpecialName);
       System.out.println(enemy.getName() + " / " + enemy.getHP() + " / " + enemy.getSpecial() + " " + enemy.getSpecialName);
       System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
-      response = userInput.nextLine();
-      response.toLowerCase();
-      if (response.equals("a") || response.equals("attack")){
-        player.attack(enemy);
-      }
-      if (response.equals("sp") || response.equals("special")){
-        player.specialAttack(enemy);
-      }
-      if (response.equals("su") || response.equals("support")){
-        player.support()
+      helper();
+      if (enemy.getHP() <= 0){
+        System.out.println(player.getName() + "prevails over the defeated " + other.getName() + "!");
       }
     }
   }
