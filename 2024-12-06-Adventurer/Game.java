1c035f81da4f4c0b54cec12f8d74db2eb24539cb
Andy Li <ali60@stuy.edu>
2024-12-09 10:52:47 -0500

    started the while loop and adding booleans for different actions that depend on the user input

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,9 +1,25 @@
 public class Game{
   public static void main(String[] args){
-    Scanner user = new Scanner(System.in);
+    Scanner userInput = new Scanner(System.in);
     System.out.println("Enter player name below");
-    String username = userInput.nextLine();
+    String response = userInput.nextLine();
     Adventurer player = new Archer(username, 15);
     Adventurer enemy = new CodeWarrior("Syntax Error", 8);
+    while (!response.equals("quit")){
+      System.out.println(player.getName() + " / " + player.getHP() + " / " + player.getSpecial() + " " + player.getSpecialName);
+      System.out.println(enemy.getName() + " / " + enemy.getHP() + " / " + enemy.getSpecial() + " " + enemy.getSpecialName);
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+      response = userInput.nextLine();
+      response.toLowerCase();
+      if (response.equals("a") || response.equals("attack")){
+        player.attack(enemy);
+      }
+      if (response.equals("sp") || response.equals("special")){
+        player.specialAttack(enemy);
+      }
+      if (response.equals("su") || response.equals("support")){
+        player.support()
+      }
+    }
   }
 }
