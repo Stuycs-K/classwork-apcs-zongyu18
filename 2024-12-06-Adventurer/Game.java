e7cfe33fa6283394d3331dc44bb0b2c89a0e6c5c
Andy Li <ali60@stuy.edu>
2024-12-10 00:31:35 -0500

    fixed syntax errors regarding the Scanner class

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,16 +1,22 @@
+import java.util.Scanner;
+
 public class Game{
   public static boolean helper(){
     System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+    Scanner userInput = new Scanner(System.in);
     String response = userInput.nextLine();
     response.toLowerCase();
     if (response.equals("a") || response.equals("attack")){
       player.attack(enemy);
+      return true;
     }
     else if (response.equals("sp") || response.equals("special")){
       player.specialAttack(enemy);
+      return true;
     }
     else if (response.equals("su") || response.equals("support")){
-      player.support()
+      player.support();
+      return true;
     }
     else if (response.equals("quit")){
       return false;
@@ -24,14 +30,16 @@ public class Game{
     Scanner userInput = new Scanner(System.in);
     System.out.println("Enter player name below");
     String response = userInput.nextLine();
-    Adventurer player = new Archer(username, 15);
+    Adventurer player = new Archer(response, 15);
     Adventurer enemy = new CodeWarrior("Syntax Error", 8);
     boolean toContinue = true;
-    while (helper()){
-      System.out.println(player.getName() + " / " + player.getHP() + " / " + player.getSpecial() + " " + player.getSpecialName);
-      System.out.println(enemy.getName() + " / " + enemy.getHP() + " / " + enemy.getSpecial() + " " + enemy.getSpecialName);
+    boolean bothAlive = true;
+    while (helper() && bothAlive){
+      System.out.println(player.getName() + " / " + player.getHP() + " / " + player.getSpecial() + " " + player.getSpecialName());
+      System.out.println(enemy.getName() + " / " + enemy.getHP() + " / " + enemy.getSpecial() + " " + enemy.getSpecialName());
       if (enemy.getHP() <= 0){
         System.out.println(player.getName() + "prevails over the defeated " + enemy.getName() + "!");
+        bothAlive = false;
       }
       int botChoice = (int) (Math.random() * 3);
       if (botChoice == 0){
@@ -45,6 +53,7 @@ public class Game{
       }
       if (player.getHP() <= 0){
         System.out.println(enemy.getName() + "prevails over the defeated " + player.getName() + "!");
+        bothAlive = false;
       }
     }
   }
