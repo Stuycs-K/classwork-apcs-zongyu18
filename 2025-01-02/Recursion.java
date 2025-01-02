14386141d2666e30ceba84692792636bcc693c81
Andy Li <ali60@stuy.edu>
2025-01-02 10:55:31 -0500

    tested simple cases for n, found the largest n such that fib(n) doesn't int overflow, and created the timing code

--- 2025-01-02/Recursion.java
+++ 2025-01-02/Recursion.java
@@ -1,5 +1,5 @@
 public class Recursion{
-  public static int fib(n){
+  public static int fib(int n){
     if (n < 2){
       return n;
     }
@@ -8,6 +8,20 @@ public class Recursion{
     }
   }
   public static void main(String[] args){
+    /* Testing simple cases for n
+    System.out.println(fib(0));
+    System.out.println(fib(1));
+    System.out.println(fib(2));
+    System.out.println(fib(3));
+    System.out.println(fib(4));
+    System.out.println(fib(5));
+    System.out.println(fib(6));
+    System.out.println(fib(7));
+    System.out.println(fib(10));*/
 
+    //46 is the largest n such that fib(n) fits in an int
+    //System.out.println(fib(47));
+
+    fib(args[0]);
   }
 }
