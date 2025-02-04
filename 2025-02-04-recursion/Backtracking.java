32105144f6c7de5ae6332c0927c08254ac7de7c2
Andy Li <ali60@stuy.edu>
2025-02-04 11:01:21 -0500

    added code for the edge case of the first method

--- 2025-02-04-recursion/Backtracking.java
+++ 2025-02-04-recursion/Backtracking.java
@@ -1,18 +1,23 @@
 public class Backtracking{
   public static long countNoDoubleLetterWords(int length,String word, String letters){
     if (length == 0){
-      return 1;
+      return 0;
     }
     else{
       for (int count = 0; count < letters.length(); count++){
-        if (letters.charAt(count) != word.charAt(word.length() - 1){
-          return (length, word + letters.charAt(count), letters);
+        if (word.length() == 0){
+          return 1 + countNoDoubleLetterWords(length - 1, word + letters.charAt(count), letters);
+        }
+        else if (letters.charAt(count) != word.charAt(word.length() - 1)){
+          return 1 + countNoDoubleLetterWords(length - 1, word + letters.charAt(count), letters);
         }
       }
     }
+    return 0;
   }
 
-  public static void main(String[] args){
 
+  public static void main(String[] args){
+    System.out.println((countNoDoubleLetterWords(1, "", "abcd")));
   }
 }
