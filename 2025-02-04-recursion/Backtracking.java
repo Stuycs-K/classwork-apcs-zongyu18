aa4012e1d9ea7fd097e98df8d69e37471e0a7357
Andy Li <ali60@stuy.edu>
2025-02-04 13:03:46 -0500

    realized my code was completely wrong because it only returns one leaf of the whole tree; redid method right this time

--- 2025-02-04-recursion/Backtracking.java
+++ 2025-02-04-recursion/Backtracking.java
@@ -1,23 +1,26 @@
 public class Backtracking{
   public static long countNoDoubleLetterWords(int length,String word, String letters){
+    int tracker = 0;
     if (length == 0){
-      return 0;
+      return 1;
     }
     else{
       for (int count = 0; count < letters.length(); count++){
         if (word.length() == 0){
-          return 1 + countNoDoubleLetterWords(length - 1, word + letters.charAt(count), letters);
+          tracker += countNoDoubleLetterWords(length - 1, word + letters.charAt(count), letters);
         }
-        else if (letters.charAt(count) != word.charAt(word.length() - 1)){
-          return 1 + countNoDoubleLetterWords(length - 1, word + letters.charAt(count), letters);
+        else{
+          if (word.charAt(word.length() - 1) != letters.charAt(count)){
+            tracker += countNoDoubleLetterWords(length - 1, word + letters.charAt(count), letters);
+          }
         }
       }
     }
-    return 0;
+    return tracker;
   }
 
 
   public static void main(String[] args){
-    System.out.println((countNoDoubleLetterWords(1, "", "abcd")));
+    System.out.println((countNoDoubleLetterWords(3, "", "abcde")));
   }
 }
